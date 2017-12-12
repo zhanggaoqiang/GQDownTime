db758bd623ca24dc58e427ea6b24a0966ee6f42b
张高强 <835389423@qq.com>
2017-12-12 16:32:45 +0800

    替换文件

--- GQDownTime/GQDownTime.h
+++ GQDownTime/GQDownTime.h
@@ -5,15 +5,16 @@
 //  Created by 张高强 on 2017/12/12.
 //  Copyright © 2017年 eamon. All rights reserved.
 //
+//
+//#import <Foundation/Foundation.h>
+//
+//typedef void(^EndTime)(void);
+//typedef void(^WorkTime)(NSInteger work);
+//@interface GQDownTime : NSObject
+//@property(nonatomic,copy)EndTime endtime;
+//@property(nonatomic,copy)WorkTime work;
+//
+//+(instancetype)sharedGQDownTime;
+//-(void)initWithTime:(int)time;
+//@end
 
-#import <Foundation/Foundation.h>
-
-typedef void(^EndTime)(void);
-typedef void(^WorkTime)(NSInteger work);
-@interface GQDownTime : NSObject
-@property(nonatomic,copy)EndTime endtime;
-@property(nonatomic,copy)WorkTime work;
-
-+(instancetype)sharedGQDownTime;
--(void)initWithTime:(int)time;
-@end
